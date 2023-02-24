3149238fa67e8ea629aff6506c925fd4a336b800
Emi <emilia.marczyk@rumblefish.pl>
2023-02-24 12:23:19 +0100

    Test sending large files with custom libp2p instances and websockets transport

--- test.js
+++ test.js
@@ -2,6 +2,7 @@ const tmp = require('tmp')
 const getPort = require('get-port')
 const fs = require('fs')
 const crypto = require('crypto')
+const { Multiaddr } = require('multiaddr')
 
 const sleep = async (time = 1000) =>
   await new Promise(resolve => {
@@ -84,9 +85,26 @@ const downloadFile = async (ipfsInstance, cid, filename) => {
   console.log('after iterating')
 }
 
+async function createPeerId() {
+  const PeerId = await import('peer-id')
+  const { peerIdFromKeys } = await eval("import('@libp2p/peer-id')")
+  const peerId = await PeerId.create()
+  // @eslint-ignore
+  return peerIdFromKeys(peerId.marshalPubKey(), peerId.marshalPrivKey())
+}
+
 const main = async () => {
   const { create } = await import('ipfs-core')
   const {createInstance} = (await import('orbit-db')).default
+  const { createLibp2p } = await import('libp2p')
+  const { noise } = await import('@chainsafe/libp2p-noise')
+  const { gossipsub }  = await import('@chainsafe/libp2p-gossipsub')
+  const { mplex } = await import('@libp2p/mplex')
+  const { kadDHT } = await import('@libp2p/kad-dht')
+  const { webSockets } = await import('@libp2p/websockets')
+  const filters = await import('@libp2p/websockets/filters')
+
+  
   
   const dir1 = createTmpDir()
   const dir2 = createTmpDir()
@@ -99,7 +117,79 @@ const main = async () => {
   const port7 = await getPort()
   const port8 = await getPort()
 
+  const port9 = await getPort()
+  const port10 = await getPort()
+
+  const peerId1 = await createPeerId()
+  const peerId2 = await createPeerId()
+
+  // Custom Libp2p instances
+  const p2p1 = await createLibp2p({
+    connectionManager: {
+      minConnections: 3,
+      maxConnections: 8,
+      dialTimeout: 120_000,
+      maxParallelDials: 10
+    },
+    peerId: peerId1,
+    addresses: {
+      listen: [`/ip4/127.0.0.1/tcp/${port9}/ws`]
+    },
+    streamMuxers: [mplex()],
+    connectionEncryption: [noise()],
+    relay: {
+      enabled: false,
+      hop: {
+        enabled: true,
+        active: false
+      }
+    },
+    transports: [
+      webSockets({
+        filter: filters.all
+      })],
+    dht: kadDHT(),
+    pubsub: gossipsub({ allowPublishToZeroPeers: true }),
+  })
+
+  const p2p2 = await createLibp2p({
+    connectionManager: {
+      minConnections: 3,
+      maxConnections: 8,
+      dialTimeout: 120_000,
+      maxParallelDials: 10
+    },
+    peerId: peerId2,
+    addresses: {
+      listen: [`/ip4/127.0.0.1/tcp/${port10}/ws`]
+    },
+    streamMuxers: [mplex()],
+    connectionEncryption: [noise()],
+    relay: {
+      enabled: false,
+      hop: {
+        enabled: true,
+        active: false
+      }
+    },
+    transports: [
+      webSockets({
+        filter: filters.all
+      })],
+    dht: kadDHT(),
+    pubsub: gossipsub({ allowPublishToZeroPeers: true }),
+  })
+
+  // Custom IPFS instances
   const ipfs1 = await create({
+    libp2p: async () => p2p1,
+    preload: { enabled: false },
+    EXPERIMENTAL: {
+      ipnsPubsub: true
+    },
+    init: {
+      privateKey: peerId1
+    },
     repo: dir1.name,
     config: {
       Addresses: {
@@ -111,6 +201,14 @@ const main = async () => {
   })
 
   const ipfs2 = await create({
+    libp2p: async () => p2p2,
+    preload: { enabled: false },
+    EXPERIMENTAL: {
+      ipnsPubsub: true
+    },
+    init: {
+      privateKey: peerId2
+    },
     repo: dir2.name,
     config: {
       Addresses: {
@@ -123,6 +221,9 @@ const main = async () => {
 
   let orbitdb1 = await createInstance(ipfs1, { directory: dir1.name })
   let orbitdb2 = await createInstance(ipfs2, { directory: dir2.name })
+  
+  console.log('Peer1 is dialing peer2 by address')
+  await p2p1.dial(new Multiaddr(`/ip4/127.0.0.1/tcp/${port10}/ws`))
 
   let kev = await orbitdb1.log('kev', {
     accessController: {
@@ -141,24 +242,27 @@ const main = async () => {
   })
   
   kev2.events.on('write', () => {
-    console.log('kev2 Adding message')
+    console.log('kev2 wrote message')
   })
   
   kev.events.on('replicate.progress', (address, _hash, entry, progress, total) => {
-    console.log('kev replicated something')
-    console.log(Date.now() - entry.payload.value.timestamp)
+    console.log('kev replicated:', entry.payload.value)
   })
   
   kev2.events.on('replicate.progress', (address, _hash, entry, progress, total) => {
-    console.log('kev2 replicated something')
-    console.log(Date.now() - entry.payload.value.timestamp)
+    console.log('kev2 replicated:', entry.payload.value)
   })
 
-  const filename = 'largeFile.txt'
-  createFile(filename, 73400320) // 70MB
+  const filename = `${(Math.random() + 1).toString(36).substring(7)}_largeFile.txt`
+  createFile(filename, 73400320) // 70MB    
   const cid = await uploadFile(ipfs1, filename)
   
   await sleep(20000)
+  // Send some messages to prove that peers are connected and replicating
+  await kev.add('testEntry1')
+  await kev.add('testEntry2')
+  await kev2.add('testEntry3')
+  
   await downloadFile(ipfs2, cid, filename)
   await sleep(20000)
   
